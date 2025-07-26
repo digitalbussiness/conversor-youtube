03f20d0d629c79ca04d7453b840bc3f774afe17b
digitalbussiness <digitalbussiness331@gmail.com>
2025-07-25 23:56:25 -0300

    Update and rename sw.js(3) to sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
 function getYmid() {
-  try { return new URL(location.href).searchParams.get('ymid'); }
-  catch (e) { console.warn(e); }
+  try {
+    return new URL(location.href).searchParams.get('ymid');
+  } catch (e) {
+    console.warn(e);
+  }
   return null;
 }
 
 function getVar() {
-  try { return new URL(location.href).searchParams.get('var'); }
-  catch (e) { console.warn(e); }
+  try {
+    return new URL(location.href).searchParams.get('var');
+  } catch (e) {
+    console.warn(e);
+  }
   return null;
 }
 
@@ -19,5 +25,4 @@ self.options = {
 };
 
 self.lary = "";
-
 importScripts('https://desekansr.com/act/files/sw.perm.check.min.js?r=sw');
