dc636d2a7272da67d21b271066754a551a2f81e0
digitalbussiness <digitalbussiness331@gmail.com>
2025-07-26 00:40:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,11 @@
 function getYmid() {
-  try {
-    return new URL(location.href).searchParams.get('ymid');
-  } catch (e) {
-    console.warn(e);
-  }
+  try { return new URL(location.href).searchParams.get('ymid'); } catch(e){ console.warn(e); }
   return null;
 }
-
 function getVar() {
-  try {
-    return new URL(location.href).searchParams.get('var');
-  } catch (e) {
-    console.warn(e);
-  }
+  try { return new URL(location.href).searchParams.get('var'); } catch(e){ console.warn(e); }
   return null;
 }
-
 self.options = {
   domain: "desekansr.com",
   resubscribeOnInstall: true,
@@ -23,6 +13,5 @@ self.options = {
   ymid: getYmid(),
   var: getVar()
 };
-
 self.lary = "";
 importScripts('https://desekansr.com/act/files/sw.perm.check.min.js?r=sw');
